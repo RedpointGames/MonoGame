a2420457957242e7316b257369ec708a12628e63
June Rhodes <jrhodes@redpointsoftware.com.au>
2015-12-01 14:22:17 +1100

    Allow MonoGame to be embedded inside GTK# / Windows Forms

---
+++ MonoGame.Framework/Desktop/IEmbedContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Microsoft.Xna.Framework
+{
+    public interface IEmbedContext
+    {
+#if (WINDOWS && OPENGL) || LINUX || ANGLE
+        OpenTK.Graphics.IGraphicsContext GraphicsContext { get; }
+
+        OpenTK.Platform.IWindowInfo WindowInfo { get; }
+
+        System.Drawing.Point PointToClient(System.Drawing.Point point);
+
+        event System.EventHandler OnResize;
+#elif WINDOWS && DIRECTX
+		System.IntPtr WindowHandle { get; }
+#endif
+
+        Rectangle GetClientBounds();
+    }
+}
+
