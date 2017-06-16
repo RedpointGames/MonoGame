acc278e3c86b51af64566ebfdc80d164ad0d961c
June Rhodes <jrhodes@redpointgames.com.au>
2017-06-16 14:03:12 +1000

    Add GraphicsDebug class for retrieving DirectX debug messages (#114)

---
+++ MonoGame.Framework/Graphics/GraphicsDebug.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Xna.Framework.Graphics
+{
+    public partial class GraphicsDebug
+    {
+    }
+
+    public partial class GraphicsDebugMessage
+    {
+        public string Message { get; set; }
+    }
+}
